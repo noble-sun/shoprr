cf3f3a67a3539a094ef1ba304f58fe0a37cf1ef4
noble-sun <matsubayashi.helio+github@gmail.com>
2025-02-16 16:06:36 -0300

    feat: add some model validations for cart and cart_item

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,14 +2,20 @@ class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
 
-  validates :quantity, :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validate :stock_availabillity
+ 
+  before_save :update_price, unless: -> { will_save_change_to_price? }
 
   private
 
   def stock_availabillity
     if product && (quantity > product.quantity)
-      errors.add(:quantity, "Not enough stock available of this product")
+      errors.add(:quantity, "insufficient for this product")
     end
+  end
+
+  def update_price
+    self.price = quantity * product.price
   end
 end
