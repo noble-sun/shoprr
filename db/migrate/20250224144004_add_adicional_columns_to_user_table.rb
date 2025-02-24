2c59c826e6b3ab23a64ef4bd5b490520968bef37
noble-sun <matsubayashi.helio+github@gmail.com>
2025-02-24 15:32:20 -0300

    style: remove whitetspace

--- db/migrate/20250224144004_add_adicional_columns_to_user_table.rb
+++ db/migrate/20250224144004_add_adicional_columns_to_user_table.rb
@@ -1,7 +1,7 @@
 class AddAdicionalColumnsToUserTable < ActiveRecord::Migration[8.0]
   def change
-    add_column :users, :name, :string, null: false 
-    add_column :users, :surname, :string, null: false 
+    add_column :users, :name, :string, null: false
+    add_column :users, :surname, :string, null: false
     add_column :users, :phone, :string
     add_column :users, :date_of_birth, :date
     add_column :users, :active, :boolean, null: false, default: true
