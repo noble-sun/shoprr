4fb65a9789c4e74bd3fbcf4dba9127893b35df1a
noble-sun <matsubayashi.helio+github@gmail.com>
2025-03-07 20:35:38 -0300

    style: rubocop offences

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,3 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-
-#User.create!(email_address: 'hym@email.com', cpf: '12312312312', password: '123123')
--- spec/factories/identity_providers.rb
+++ spec/factories/identity_providers.rb
@@ -4,8 +4,8 @@ FactoryBot.define do
     name { 'google' }
     account_identifier { 'google-sub-1234-aasdf' }
     access_token { 'google-access-token' }
-    id_token { 'id-token-1234asddf'}
+    id_token { 'id-token-1234asddf' }
     expires_in { DateTime.now + 1.hour }
-    refresh_token { 'google-refresh-token-123asdf'}
+    refresh_token { 'google-refresh-token-123asdf' }
   end
 end
